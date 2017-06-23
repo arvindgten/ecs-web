72c790c38afe68634a24e022cd108cab396bb7d9
Raghu Raman <rraagghhu@gmail.com>
2017-06-23 18:21:28 +0530

    Updating server.js

--- server.js
+++ server.js
@@ -10,9 +10,21 @@ const PORT = 8080;
 // App
 const app = express();
 app.get( '/*', function( req, res ) {
-	// var content = fs.readFileSync( 'test.html', 'utf8' );
-	console.log( "yolo" );
-	res.send( "No sir!" );
+	var content = null;
+	if( req.path == '/pwa-sw-GAMMA_HINDI.js' ) {
+		content = fs.readFileSync( 'src/pwa-service-worker/pwa-sw-GAMMA_HINDI.js' );
+		res.set( 'Content-Type', 'text/javascript' );
+	} else if( req.path == '/pwa-stylesheets/css/style.css' ) {
+		content = fs.readFileSync( 'src/pwa-stylesheets/style.css' );
+		res.set( 'Content-Type', 'text/css' );
+	} else if( req.path == '/health' ) {
+		content = Date.now() + "";
+		res.set( 'Content-Type', 'text/plain' );
+	} else {
+		content = fs.readFileSync( 'src/pwa-markup/PWA-GAMMA_HINDI.html', 'utf8' );
+		res.set( 'Content-Type', 'text/html' );
+	}
+	res.send( content );
 });
 
 app.listen( PORT );
