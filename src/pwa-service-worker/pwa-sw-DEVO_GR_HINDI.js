66b5d1dbffd94703cb0d770bb7e7fa41a9e1e864
Raghu Raman <rraagghhu@gmail.com>
2017-07-27 10:31:16 +0530

    Updating PWA files

--- src/pwa-service-worker/pwa-sw-DEVO_GR_HINDI.js
+++ src/pwa-service-worker/pwa-sw-DEVO_GR_HINDI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?240720171520',
+		'pwa-stylesheets/css/style.css?270720171031',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -27,7 +27,7 @@
 		'https://www.ptlp.co/resource-all/font/font-hi.css'
 	];
 
-	var STATIC_VERSION = "240720171520";
+	var STATIC_VERSION = "270720171031";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
