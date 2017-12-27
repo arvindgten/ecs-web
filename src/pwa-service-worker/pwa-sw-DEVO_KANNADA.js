a0d9fc0770c66a6c6b2bf5f78646ba1c38e81ca8
Raghu Raman <rraagghhu@gmail.com>
2017-12-27 17:55:12 +0530

    Updating PWA files

--- src/pwa-service-worker/pwa-sw-DEVO_KANNADA.js
+++ src/pwa-service-worker/pwa-sw-DEVO_KANNADA.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?221220172033',
+		'pwa-stylesheets/css/style.css?271220171755',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-kn.css'
 	];
 
-	var STATIC_VERSION = "221220172033";
+	var STATIC_VERSION = "271220171755";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://kannada-devo.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-221220172033.html";
+	var PWA_INDEX_HTML = "app-shell-271220171755.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
