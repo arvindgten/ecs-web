4d91ac7ffb269645c55763dbb940ac70c52a4262
Raghu Raman <rraagghhu@gmail.com>
2018-02-09 21:04:20 +0530

    Updating PWA files

--- src/pwa-service-worker/pwa-sw-MARATHI.js
+++ src/pwa-service-worker/pwa-sw-MARATHI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?090220181837',
+		'pwa-stylesheets/css/style.css?090220182104',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-mr.css'
 	];
 
-	var STATIC_VERSION = "090220181837";
+	var STATIC_VERSION = "090220182104";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://marathi.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-090220181837.html";
+	var PWA_INDEX_HTML = "app-shell-090220182104.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
