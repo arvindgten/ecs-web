71b737f8c199c0df6f205c733452eda0b96fa701
Jarvis <jarvis@pratilipi.com>
2018-02-22 12:31:24 +0000

    Updating PWA files built from pratilipi repository.

--- src/pwa-service-worker/pwa-sw-GAMMA_GUJARATI_GR.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_GUJARATI_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?220220180725',
+		'pwa-stylesheets/css/style.css?220220181230',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-gu.css'
 	];
 
-	var STATIC_VERSION = "220220180725";
+	var STATIC_VERSION = "220220181230";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://gujarati-gamma-gr.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-220220180725.html";
+	var PWA_INDEX_HTML = "app-shell-220220181230.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
