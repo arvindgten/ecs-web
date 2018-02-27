91ef1bbb730e13aa6295a90f467d873b3c9b64cc
Jarvis <jarvis@pratilipi.com>
2018-02-27 12:47:18 +0000

    Updating PWA files built from pratilipi repository.

--- src/pwa-service-worker/pwa-sw-DEVO_TAMIL.js
+++ src/pwa-service-worker/pwa-sw-DEVO_TAMIL.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?270220181233',
+		'pwa-stylesheets/css/style.css?270220181246',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-ta.css'
 	];
 
-	var STATIC_VERSION = "270220181233";
+	var STATIC_VERSION = "270220181246";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://tamil-devo.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-270220181233.html";
+	var PWA_INDEX_HTML = "app-shell-270220181246.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
