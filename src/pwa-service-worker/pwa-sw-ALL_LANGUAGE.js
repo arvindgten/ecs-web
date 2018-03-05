b8a5b50555bf34e85b136effc182b82cc6ac58cc
Raghu Raman <rraagghhu@gmail.com>
2018-03-05 19:29:18 +0530

    Updating PWA files from 6fdba1bdd86cbfecff873ba3bcb064474f0d92f0

--- src/pwa-service-worker/pwa-sw-ALL_LANGUAGE.js
+++ src/pwa-service-worker/pwa-sw-ALL_LANGUAGE.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?020320181626',
+		'pwa-stylesheets/css/style.css?050320181929',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-en.css'
 	];
 
-	var STATIC_VERSION = "020320181626";
+	var STATIC_VERSION = "050320181929";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://www.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-020320181626.html";
+	var PWA_INDEX_HTML = "app-shell-050320181929.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
