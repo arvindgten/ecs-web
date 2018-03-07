1a4688589619f4711308f8bf1c66da7e2e111c83
Raghu Raman <rraagghhu@gmail.com>
2018-03-07 14:18:29 +0530

    Updating service.

--- server.js
+++ server.js
@@ -190,9 +190,6 @@ app.use( cookieParser() );
 // gzip all responses
 app.use( compression() );
 
-// prerender
-// app.use(require('prerender-node'));
-
 // Health
 app.get( '/health', (req, res, next) => {
 	console.log( "Healthy!" );
