b66367a2ce7a17cdbc07c875d90245ebea821aa4
Jarvis <jarvis@pratilipi.com>
2018-03-08 14:55:10 +0530

    Updating PWA files from 50c543540e398a1f6c2f9da242d19e4bfe98ba31

--- src/pwa-service-worker/pwa-sw-ALPHA.js
+++ src/pwa-service-worker/pwa-sw-ALPHA.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?080320181237',
+		'pwa-stylesheets/css/style.css?080320181455',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-hi.css'
 	];
 
-	var STATIC_VERSION = "080320181237";
+	var STATIC_VERSION = "080320181455";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://www-delta.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-080320181237.html";
+	var PWA_INDEX_HTML = "app-shell-080320181455.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
