82bc942c28af7b6c32e4ef7cf216b93c0169667e
Jarvis <jarvis@pratilipi.com>
2018-03-19 17:57:25 +0530

    Updating PWA files from 4fd46512b0779b27747fd8a9b3f5014f78d7bc78

--- src/pwa-service-worker/pwa-sw-GAMMA_KANNADA.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_KANNADA.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?190320181742',
+		'pwa-stylesheets/css/style.css?190320181757',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-kn.css'
 	];
 
-	var STATIC_VERSION = "190320181742";
+	var STATIC_VERSION = "190320181757";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://kannada-gamma.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-190320181742.html";
+	var PWA_INDEX_HTML = "app-shell-190320181757.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
