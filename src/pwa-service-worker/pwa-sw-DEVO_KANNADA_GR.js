987f04ca317754e0ca405fad6637b9acfd7fc451
Jarvis <jarvis@pratilipi.com>
2018-03-26 15:10:56 +0530

    Updating PWA files from 88f13e0f86bc87541cfea4f6111a9bf1ad96d7f3

--- src/pwa-service-worker/pwa-sw-DEVO_KANNADA_GR.js
+++ src/pwa-service-worker/pwa-sw-DEVO_KANNADA_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?260320181509',
+		'pwa-stylesheets/css/style.css?260320181510',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-kn.css'
 	];
 
-	var STATIC_VERSION = "260320181509";
+	var STATIC_VERSION = "260320181510";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://kannada-devo-gr.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-260320181509.html";
+	var PWA_INDEX_HTML = "app-shell-260320181510.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
