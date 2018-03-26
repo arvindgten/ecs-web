25e572aef6690f36a77865d1ed882bdc8d520fcd
Jarvis <jarvis@pratilipi.com>
2018-03-26 18:25:21 +0530

    Updating PWA files from f0a9b06e1114a03b3cde66d7bff5625176cb924c

--- src/pwa-service-worker/pwa-sw-GAMMA_TAMIL_GR.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_TAMIL_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?260320181823',
+		'pwa-stylesheets/css/style.css?260320181825',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-ta.css'
 	];
 
-	var STATIC_VERSION = "260320181823";
+	var STATIC_VERSION = "260320181825";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://tamil-gamma-gr.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-260320181823.html";
+	var PWA_INDEX_HTML = "app-shell-260320181825.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
