8855d4cf22f9c06a907dad7e854dabf0ee6ef484
Jarvis <jarvis@pratilipi.com>
2018-04-02 21:44:13 +0530

    Updating PWA files from ac7545bc172ff29fc9ed97d75be81f52159b945e

--- src/pwa-service-worker/pwa-sw-GAMMA_TELUGU_GR.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_TELUGU_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?020420182014',
+		'pwa-stylesheets/css/style.css?020420182144',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-te.css'
 	];
 
-	var STATIC_VERSION = "020420182014";
+	var STATIC_VERSION = "020420182144";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://telugu-gamma-gr.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-020420182014.html";
+	var PWA_INDEX_HTML = "app-shell-020420182144.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
