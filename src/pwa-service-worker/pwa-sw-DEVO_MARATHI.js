78d5a59eba794cbf4016eb1f1f9260ae9041c10c
Jarvis <jarvis@pratilipi.com>
2018-04-06 20:28:11 +0530

    Updating PWA files from 420a10902174e4647368973ef889a6e0589a997d

--- src/pwa-service-worker/pwa-sw-DEVO_MARATHI.js
+++ src/pwa-service-worker/pwa-sw-DEVO_MARATHI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?060420182020',
+		'pwa-stylesheets/css/style.css?060420182028',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-mr.css'
 	];
 
-	var STATIC_VERSION = "060420182020";
+	var STATIC_VERSION = "060420182028";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://marathi-devo.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-060420182020.html";
+	var PWA_INDEX_HTML = "app-shell-060420182028.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
