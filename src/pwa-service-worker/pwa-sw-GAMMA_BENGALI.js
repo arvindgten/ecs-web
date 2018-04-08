d2545740367b840431fed5f8df306b954d3dc2c8
Jarvis <jarvis@pratilipi.com>
2018-04-08 16:32:43 +0530

    Updating PWA files from 29854b10bf8483a1aa536be2eb49d7aa0b72ea2e

--- src/pwa-service-worker/pwa-sw-GAMMA_BENGALI.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_BENGALI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?080420181631',
+		'pwa-stylesheets/css/style.css?080420181632',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-bn.css'
 	];
 
-	var STATIC_VERSION = "080420181631";
+	var STATIC_VERSION = "080420181632";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://bengali-gamma.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-080420181631.html";
+	var PWA_INDEX_HTML = "app-shell-080420181632.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
