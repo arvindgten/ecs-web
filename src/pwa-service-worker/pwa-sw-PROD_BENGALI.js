7cdf64e8a86a7ec80086d03f41074c5d715b4e1c
Jarvis <jarvis@pratilipi.com>
2018-04-09 19:55:28 +0530

    Updating PWA files from d96d0e5461103867d5d497c923a6773bc7858ee2

--- src/pwa-service-worker/pwa-sw-PROD_BENGALI.js
+++ src/pwa-service-worker/pwa-sw-PROD_BENGALI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?090420181633',
+		'pwa-stylesheets/css/style.css?090420181955',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-bn.css'
 	];
 
-	var STATIC_VERSION = "090420181633";
+	var STATIC_VERSION = "090420181955";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://bengali-mum.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-090420181633.html";
+	var PWA_INDEX_HTML = "app-shell-090420181955.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
