bcba7ac8e5ffd649792c5c4d272fd76577abe478
Jarvis <jarvis@pratilipi.com>
2018-04-11 11:26:58 +0530

    Updating PWA files from bc4c0d2b37696b8cef1a9850ed09dbc684917ef0

--- src/pwa-service-worker/pwa-sw-PROD_KANNADA.js
+++ src/pwa-service-worker/pwa-sw-PROD_KANNADA.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?110420181104',
+		'pwa-stylesheets/css/style.css?110420181126',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-kn.css'
 	];
 
-	var STATIC_VERSION = "110420181104";
+	var STATIC_VERSION = "110420181126";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://kannada-mum.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-110420181104.html";
+	var PWA_INDEX_HTML = "app-shell-110420181126.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
