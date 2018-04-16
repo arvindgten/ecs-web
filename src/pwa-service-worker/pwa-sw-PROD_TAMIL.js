193e8df8808112e692d954af0bf3b9b8f67d2e8e
Jarvis <jarvis@pratilipi.com>
2018-04-16 15:03:53 +0530

    Updating PWA files from 23a87db203c0251a63aca26819eb614fb114f7a3

--- src/pwa-service-worker/pwa-sw-PROD_TAMIL.js
+++ src/pwa-service-worker/pwa-sw-PROD_TAMIL.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?160420181437',
+		'pwa-stylesheets/css/style.css?160420181503',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-ta.css'
 	];
 
-	var STATIC_VERSION = "160420181437";
+	var STATIC_VERSION = "160420181503";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://tamil-mum.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-160420181437.html";
+	var PWA_INDEX_HTML = "app-shell-160420181503.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
