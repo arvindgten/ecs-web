98da3c75dcf6e35d8c68e19085f3f9a836692429
Jarvis <jarvis@pratilipi.com>
2018-04-17 02:53:02 +0530

    Updating PWA files from 00466ac9697dbdac698c4680f66ef9420848484c

--- src/pwa-service-worker/pwa-sw-ALL_LANGUAGE.js
+++ src/pwa-service-worker/pwa-sw-ALL_LANGUAGE.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?160420181623',
+		'pwa-stylesheets/css/style.css?170420180252',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-en.css'
 	];
 
-	var STATIC_VERSION = "160420181623";
+	var STATIC_VERSION = "170420180252";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://www.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-160420181623.html";
+	var PWA_INDEX_HTML = "app-shell-170420180252.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
