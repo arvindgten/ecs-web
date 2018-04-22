26b47d5e3a16b3f5aed499094fbd25caa4ffcc14
Jarvis <jarvis@pratilipi.com>
2018-04-23 02:14:42 +0530

    Updating PWA files from 3ba8872b219367cef3a06a8c2196b21614b730fa

--- src/pwa-service-worker/pwa-sw-MALAYALAM.js
+++ src/pwa-service-worker/pwa-sw-MALAYALAM.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?200420181333',
+		'pwa-stylesheets/css/style.css?230420180214',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-ml.css'
 	];
 
-	var STATIC_VERSION = "200420181333";
+	var STATIC_VERSION = "230420180214";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://malayalam.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-200420181333.html";
+	var PWA_INDEX_HTML = "app-shell-230420180214.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
