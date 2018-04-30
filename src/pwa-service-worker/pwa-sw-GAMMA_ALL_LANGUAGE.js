4af28361cf7550e547181bb6a1087ddde8ce1f55
Jarvis <jarvis@pratilipi.com>
2018-04-30 16:11:20 +0530

    Updating PWA files from 3863d0817b21fcc4e1aa3a7a508449df30876eb3

--- src/pwa-service-worker/pwa-sw-GAMMA_ALL_LANGUAGE.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_ALL_LANGUAGE.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?300420181609',
+		'pwa-stylesheets/css/style.css?300420181611',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-en.css'
 	];
 
-	var STATIC_VERSION = "300420181609";
+	var STATIC_VERSION = "300420181611";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://www-gamma.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-300420181609.html";
+	var PWA_INDEX_HTML = "app-shell-300420181611.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
