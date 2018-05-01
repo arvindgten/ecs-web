3f2b357b56d97a3b7f5eead52d1638100340026f
Jarvis <jarvis@pratilipi.com>
2018-05-01 14:11:49 +0530

    Updating PWA files from dd591ce2e6557c8df9deb3f31d473fe01d00a124

--- src/pwa-service-worker/pwa-sw-GUJARATI.js
+++ src/pwa-service-worker/pwa-sw-GUJARATI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?300420181611',
+		'pwa-stylesheets/css/style.css?010520181411',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-gu.css'
 	];
 
-	var STATIC_VERSION = "300420181611";
+	var STATIC_VERSION = "010520181411";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://gujarati.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-300420181611.html";
+	var PWA_INDEX_HTML = "app-shell-010520181411.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
