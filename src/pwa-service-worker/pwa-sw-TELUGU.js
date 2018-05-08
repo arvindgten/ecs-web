b5c4aa9fb0ebd229e69b45735fd82962a86e575b
Jarvis <jarvis@pratilipi.com>
2018-05-08 14:08:46 +0530

    Updating PWA files from a0346277987cd33c069d99ce31f6a01b86d2e6a2

--- src/pwa-service-worker/pwa-sw-TELUGU.js
+++ src/pwa-service-worker/pwa-sw-TELUGU.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?070520181838',
+		'pwa-stylesheets/css/style.css?080520181408',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-te.css'
 	];
 
-	var STATIC_VERSION = "070520181838";
+	var STATIC_VERSION = "080520181408";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://telugu.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-070520181838.html";
+	var PWA_INDEX_HTML = "app-shell-080520181408.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
