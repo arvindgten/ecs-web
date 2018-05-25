02880fc5354b520424566f5f940e81e4b3dd808e
Jarvis <jarvis@pratilipi.com>
2018-05-25 13:02:43 +0530

    Updating PWA files from 506ac650600393dc24d0233ed6f828cf2cf15cd0

--- src/pwa-service-worker/pwa-sw-DEVO_MALAYALAM.js
+++ src/pwa-service-worker/pwa-sw-DEVO_MALAYALAM.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?250520181301',
+		'pwa-stylesheets/css/style.css?250520181302',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-ml.css'
 	];
 
-	var STATIC_VERSION = "250520181301";
+	var STATIC_VERSION = "250520181302";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://malayalam-devo.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-250520181301.html";
+	var PWA_INDEX_HTML = "app-shell-250520181302.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
