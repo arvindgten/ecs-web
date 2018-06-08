e96be6eaef4fda73932b6916d3363b92d19b002c
Jarvis <jarvis@pratilipi.com>
2018-06-08 16:52:47 +0530

    Updating PWA files from a2c86a0f26b64c68d14f5b8666cd96dcd2f308ba

--- src/pwa-service-worker/pwa-sw-DELTA_TELUGU.js
+++ src/pwa-service-worker/pwa-sw-DELTA_TELUGU.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?080620181650',
+		'pwa-stylesheets/css/style.css?080620181652',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-te.css'
 	];
 
-	var STATIC_VERSION = "080620181650";
+	var STATIC_VERSION = "080620181652";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://telugu-delta.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-080620181650.html";
+	var PWA_INDEX_HTML = "app-shell-080620181652.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
