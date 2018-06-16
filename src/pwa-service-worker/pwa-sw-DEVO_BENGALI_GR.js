7941eee7b5147aef7a8ccb304f7b09c9e727c386
Jarvis <jarvis@pratilipi.com>
2018-06-16 23:39:27 +0530

    Updating PWA files from 4f3b13820971605d8d63dc2e3d16f8625e71680f

--- src/pwa-service-worker/pwa-sw-DEVO_BENGALI_GR.js
+++ src/pwa-service-worker/pwa-sw-DEVO_BENGALI_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?140620181702',
+		'pwa-stylesheets/css/style.css?160620182339',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-bn.css'
 	];
 
-	var STATIC_VERSION = "140620181702";
+	var STATIC_VERSION = "160620182339";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://bengali-devo-gr.ptlp.co";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-140620181702.html";
+	var PWA_INDEX_HTML = "app-shell-160620182339.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
