fa4e1365056c241f4a6d891c8acf9446b5e0cb9c
Jarvis <jarvis@pratilipi.com>
2018-06-29 17:00:51 +0530

    Updating PWA files from d8c0e5833d39da5c2b41ad363304d11e9a813a57

--- src/pwa-service-worker/pwa-sw-GAMMA_MUM_ALL_LANGUAGE.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_MUM_ALL_LANGUAGE.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?290620181509',
+		'pwa-stylesheets/css/style.css?290620181700',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-en.css'
 	];
 
-	var STATIC_VERSION = "290620181509";
+	var STATIC_VERSION = "290620181700";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://www-mum-gamma.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-290620181509.html";
+	var PWA_INDEX_HTML = "app-shell-290620181700.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
