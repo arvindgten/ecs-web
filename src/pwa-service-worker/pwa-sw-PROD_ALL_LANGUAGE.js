fd46a8163956c58c16faace684aa8f8c0768c76c
Jarvis <jarvis@pratilipi.com>
2018-07-07 12:22:02 +0530

    Updating PWA files from 7a39ca332501ec3c21f693ebee4f389a290f1d8b

--- src/pwa-service-worker/pwa-sw-PROD_ALL_LANGUAGE.js
+++ src/pwa-service-worker/pwa-sw-PROD_ALL_LANGUAGE.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?070720181220',
+		'pwa-stylesheets/css/style.css?070720181221',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-en.css'
 	];
 
-	var STATIC_VERSION = "070720181220";
+	var STATIC_VERSION = "070720181221";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://www-mum.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-070720181220.html";
+	var PWA_INDEX_HTML = "app-shell-070720181221.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
