fa9faf09910c1f8a6e657f74a4c61117c3761ed2
Jarvis <jarvis@pratilipi.com>
2018-07-10 18:46:17 +0530

    Updating PWA files from 59c895a328847db7bbd50d2476d68d616e4b209b

--- src/pwa-service-worker/pwa-sw-DELTA_MALAYALAM.js
+++ src/pwa-service-worker/pwa-sw-DELTA_MALAYALAM.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?100720181558',
+		'pwa-stylesheets/css/style.css?100720181846',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-ml.css'
 	];
 
-	var STATIC_VERSION = "100720181558";
+	var STATIC_VERSION = "100720181846";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://malayalam-delta.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-100720181558.html";
+	var PWA_INDEX_HTML = "app-shell-100720181846.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
