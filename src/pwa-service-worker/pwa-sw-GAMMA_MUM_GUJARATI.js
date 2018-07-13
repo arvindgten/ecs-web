3802cad2882236652bb778ff59877fd8326b70be
Jarvis <jarvis@pratilipi.com>
2018-07-13 17:01:36 +0530

    Updating PWA files from 9512654f2fa5a7cb979e1c241e30b39c04eb9284

--- src/pwa-service-worker/pwa-sw-GAMMA_MUM_GUJARATI.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_MUM_GUJARATI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?130720181554',
+		'pwa-stylesheets/css/style.css?130720181701',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-gu.css'
 	];
 
-	var STATIC_VERSION = "130720181554";
+	var STATIC_VERSION = "130720181701";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://gujarati-mum-gamma.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-130720181554.html";
+	var PWA_INDEX_HTML = "app-shell-130720181701.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
