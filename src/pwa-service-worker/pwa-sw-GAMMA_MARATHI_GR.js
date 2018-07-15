2a92a3264678b6b896c9a24b1a0f24b852dd5467
Jarvis <jarvis@pratilipi.com>
2018-07-15 21:42:49 +0530

    Updating PWA files from 1c9b47671f0e61090d9732797b6f728860a0ad94

--- src/pwa-service-worker/pwa-sw-GAMMA_MARATHI_GR.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_MARATHI_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?150720182005',
+		'pwa-stylesheets/css/style.css?150720182142',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-mr.css'
 	];
 
-	var STATIC_VERSION = "150720182005";
+	var STATIC_VERSION = "150720182142";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://marathi-gamma-gr.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-150720182005.html";
+	var PWA_INDEX_HTML = "app-shell-150720182142.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
