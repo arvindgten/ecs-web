aadf58b514a13408abc356d33218bda89f9808ce
Jarvis <jarvis@pratilipi.com>
2018-07-25 15:24:33 +0530

    Updating PWA files from d124ee52ca033588fc166ee769f663e0967a4202

--- src/pwa-service-worker/pwa-sw-DELTA_GUJARATI.js
+++ src/pwa-service-worker/pwa-sw-DELTA_GUJARATI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?250720181445',
+		'pwa-stylesheets/css/style.css?250720181524',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-gu.css'
 	];
 
-	var STATIC_VERSION = "250720181445";
+	var STATIC_VERSION = "250720181524";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://gujarati-delta.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-250720181445.html";
+	var PWA_INDEX_HTML = "app-shell-250720181524.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
