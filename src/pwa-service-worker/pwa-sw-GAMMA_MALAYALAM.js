7011c0e477dd249a4d1fb7a40dee41b69cb7ab0e
Jarvis <jarvis@pratilipi.com>
2018-08-02 13:24:16 +0530

    Updating PWA files from 081f4b7b7fba3210b12a7ca0d4d830b4e8155624

--- src/pwa-service-worker/pwa-sw-GAMMA_MALAYALAM.js
+++ src/pwa-service-worker/pwa-sw-GAMMA_MALAYALAM.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?020820181314',
+		'pwa-stylesheets/css/style.css?020820181324',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-ml.css'
 	];
 
-	var STATIC_VERSION = "020820181314";
+	var STATIC_VERSION = "020820181324";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://malayalam-gamma.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-020820181314.html";
+	var PWA_INDEX_HTML = "app-shell-020820181324.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
