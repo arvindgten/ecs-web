e4c9397491baaeccb65f22e1f36109ab0d582573
Jarvis <jarvis@pratilipi.com>
2018-08-03 19:04:18 +0530

    Updating PWA files from 2e22f8f1023c3659e87cfd8001c5f1521711f742

--- src/pwa-service-worker/pwa-sw-PROD_HINDI.js
+++ src/pwa-service-worker/pwa-sw-PROD_HINDI.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?030820181900',
+		'pwa-stylesheets/css/style.css?030820181904',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-hi.css'
 	];
 
-	var STATIC_VERSION = "030820181900";
+	var STATIC_VERSION = "030820181904";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://hindi-mum.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-030820181900.html";
+	var PWA_INDEX_HTML = "app-shell-030820181904.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
